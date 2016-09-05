7ef42d3a176be147faa1cc2180737be59f5d016a
June Dela Cruz <junedelacruz@outlook.com>
2016-09-05 13:45:56 +1200

    Initial

---
+++ src/main/java/mvctest/domain/Hotel.java
@@ -0,0 +1,65 @@
+package mvctest.domain;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Hotel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String address;
+
+	@Column(nullable = false)
+	private String zip;
+
+	public Hotel() {
+	}
+
+	public Hotel(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public String getZip() {
+		return this.zip;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}	
+}
